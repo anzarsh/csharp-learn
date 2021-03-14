b05e10b30c73eb9f51a867a9ed21472f5da353c3
anzarsh <anzarsh@mail.ru>
2021-03-14 19:22:34 +0300

    remove some code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,27 +7,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // string name = Console.ReadLine();
-
-            // Console.WriteLine("Привет " + name + "!!!");
-
-            // convert
-            //string str;
-            //int a, b, result;
-
-            //console.writeline("введите первое число:");
-            //str = console.readline();
-            //a = convert.toint32(str);
-
-            //console.writeline("введите второе число:");
-            //console.writeline("введите второе число:");
-            //str = console.readline();
-            //b = convert.toint32(str);
-
-            //result = a + b;
-
-            //console.writeline("сумма чисел " + a + " и " + b + " равна " + result);
-
             // globalization
 
             string str = "1.3";
