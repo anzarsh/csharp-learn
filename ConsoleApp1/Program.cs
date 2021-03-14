5aa0019455e02ee01f895c926cfee487973d73dd
anzarsh <anzarsh@mail.ru>
2021-03-14 12:14:02 +0300

    init commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 11;
+
+            Console.WriteLine(a);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
