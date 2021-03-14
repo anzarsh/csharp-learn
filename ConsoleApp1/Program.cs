ec7169b04d63188cda2146b4b6f8d32883c6f4f5
anzarsh <anzarsh@mail.ru>
2021-03-14 19:11:01 +0300

    learn Convert class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,9 +6,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
+            // string name = Console.ReadLine();
 
-            Console.WriteLine("Привет " + name + "!!!");
+            // Console.WriteLine("Привет " + name + "!!!");
+
+            // convert
+            string str;
+            int a, b, result;
+
+            Console.WriteLine("Введите первое число:");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+
+            Console.WriteLine("Введите второе число:");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+
+            result = a + b;
+
+            Console.WriteLine("Сумма чисел " + a + " и " + b + " равна " + result);
         }
     }
 }
