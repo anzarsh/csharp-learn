6e9b82cac169d2f426b6bc54fc345c97fc0b7a17
anzarsh <anzarsh@mail.ru>
2021-03-20 00:27:17 +0300

    System.Threding.Thread.Sleep

---
+++ Wait/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Wait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0, d = 1; i < 20; i++, d *= 2)
+            {
+                Console.WriteLine(d);
+                Thread.Sleep(500);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
