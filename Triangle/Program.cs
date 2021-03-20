51f588d516b99ae4655b6ce7879be0c78b123646
anzarsh <anzarsh@mail.ru>
2021-03-20 23:09:27 +0300

    Console.Write, Triangle, double for

---
+++ Triangle/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 10; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 0; j <= 10 - i; j++)
+                {
+                    Console.Write(' ');
+                }
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 10; i >= 0; i--)
+            {
+                for (int j = 0; j <= 10 - i; j++)
+                {
+                    Console.Write(' ');
+                }
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
