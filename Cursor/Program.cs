dd4fc6d8506b2794a20dbdf44d32b6ce16a04a9b
anzarsh <anzarsh@mail.ru>
2021-03-21 15:44:07 +0300

    Draw in console with cursor

---
+++ Cursor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Cursor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 10;
+            while(true)
+            {
+                Draw(x, y);
+                ConsoleKey key = Console.ReadKey().Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        x--;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        y--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        x++;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        y++;
+                        break;
+                }
+            }
+        }
+
+        static void Draw(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("*");
+            Console.SetCursorPosition(0, 0);
+        }
+    }
+}
