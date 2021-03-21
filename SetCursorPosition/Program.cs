c4f6a98664f08011a3d3c539cfa34334f9922a53
anzarsh <anzarsh@mail.ru>
2021-03-21 15:34:24 +0300

    Console.SetCursorPosition, Random

---
+++ SetCursorPosition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace SetCursorPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random random = new Random();
+                int randX = random.Next(100);
+                int randY = random.Next(100);
+                Console.SetCursorPosition(randX, randY);
+                Console.Write("*");
+                Console.SetCursorPosition(100, 100);
+                Thread.Sleep(25);
+            }
+        }
+    }
+}
