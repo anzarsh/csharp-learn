081a4f7fca49103e76e59bbc2c268dbc56d8658a
anzarsh <anzarsh@mail.ru>
2021-03-26 16:57:32 +0300

    Index Range

---
+++ IndexRange/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IndexRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 8, 2, 8, 0, 3, 2, 5, 8 };
+
+            int[] arr2 = arr[3..6];
+
+            arr2[0] = -1;
+
+            // 2
+            Console.WriteLine(arr[3]);
+        }
+    }
+}
---
+++ RepeatValueArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace RepeatValueArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // {5, 5, 5, 5, 5, 5, 5, 5, 5, 5}
+            int[] arr1 = Enumerable.Repeat(5, 10).ToArray();
+
+            // {5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+            int[] arr2 = Enumerable.Range(5, 10).ToArray();
+
+            // {0, 0, 0, 0, 0}
+            int[] arr3 = new int[5];
+
+            // { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+            int[] arr4 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            // { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+            int[] arr5 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            // { 1, 2, 3, 4, 5 }
+            int[] arr6 = new[] { 1, 2, 3, 4, 5 };
+        }
+    }
+}
