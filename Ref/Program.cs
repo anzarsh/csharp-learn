ba34e7d7410b8da8f4dea6974795847bac314d65
anzarsh <anzarsh@mail.ru>
2021-04-03 11:45:10 +0300

    Ref 2

---
+++ Ref/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ref
+{
+    class Program
+    {
+        static ref int Foo(int[] arr)
+        {
+            return ref arr[0];
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 3;
+
+            ref int b = ref a;
+
+            b = 2;
+
+            // 2
+            Console.WriteLine(a);
+
+            /////////////////////////////
+
+            int[] f = new int[] { 3, 5, 7 };
+
+            ref int d = ref Foo(f);
+
+            d = 10;
+
+            //10
+            Console.WriteLine(f[0]);
+        }
+    }
+}
---
+++ Ref2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ref2
+{
+    class Program
+    {
+        static void RemoveByIndex<T>(ref T[] arr, int index)
+        {
+            T[] newArr = new T[arr.Length - 1];
+
+            for (int i = 0; i < index; i++)
+            {
+                newArr[i] = arr[i];
+            }
+
+            for (int i = index + 1; i < arr.Length; i++)
+            {
+                newArr[i - 1] = arr[i];
+            }
+
+            arr = newArr;
+        }
+
+        static void RemoveLast<T>(ref T[] arr)
+        {
+            RemoveByIndex(ref arr, arr.Length - 1);
+        }
+
+        static void RemoveFirst<T>(ref T[] arr)
+        {
+            RemoveByIndex(ref arr, 0);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 5, 6, 7, 0, 3 };
+
+            RemoveByIndex(ref arr, 3);
+            RemoveFirst(ref arr);
+            RemoveLast(ref arr);
+        }
+    }
+}
